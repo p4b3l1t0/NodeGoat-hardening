b83855a4b6d85c57bf52f663b144bdfe2f8f4d93
P4b3l1t0 <59421032+p4b3l1t0@users.noreply.github.com>
2023-10-23 00:24:38 +0200

    Update -A6 all.js

--- config/env/all.js
+++ config/env/all.js
@@ -10,8 +10,8 @@ let db = process.env.MONGODB_URI || "mongodb://localhost:27017/nodegoat";
 module.exports = {
     port,
     db,
-    cookieSecret: process.env.COOKIE_SECRET || "default_session_cookie_secret", // ¡Establece una variable de entorno!
-    cryptoKey: process.env.CRYPTO_KEY || "default_crypto_key", // ¡Establece una variable de entorno!
+    cookieSecret: process.env.COOKIE_SECRET
+    cryptoKey: process.env.CRYPTO_KEY
     cryptoAlgo: "aes256",
     hostName: "localhost",
     environmentalScripts: []
