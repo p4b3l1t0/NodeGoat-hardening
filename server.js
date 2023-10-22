3f729fbe760a7e17f928cd78aaf6362bd391364b
Multari-Noelia87 <58668572+Multari-Noelia87@users.noreply.github.com>
2023-10-22 20:25:55 -0300

    Implementación de protocolo HTTPS en server.js

--- server.js
+++ server.js
@@ -129,12 +129,12 @@ MongoClient.connect(db, (err, db) => {
         console.log(`Express http server listening on port ${port}`);
     });
 
-    /*
+    
     // Fix for A6-Sensitive Data Exposure
     // Use secure HTTPS protocol
     https.createServer(httpsOptions, app).listen(port, () => {
         console.log(`Express http server listening on port ${port}`);
     });
-    */
+    
 
 });
